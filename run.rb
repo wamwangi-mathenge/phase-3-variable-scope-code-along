c6f6eb8d9f9bb4a85f6d0a32962a2ba07924623e
Brian <wamwangimathenge@gmail.com>
2023-02-07 07:49:24 +0300

    Variable Scope

---
+++ run.rb
@@ -0,0 +1,36 @@
+# name = "Joe"
+
+# def greeting(name)
+    # puts "Hello, #{name}"
+# end
+
+# greeting("Sophie")
+
+# evil_monster = "Bowser"
+
+# def princess_peaches_castle
+    # puts "#{evil_monster} is trying to kidnap Princess Peach"
+# end
+
+# princess_peaches_castle
+
+
+# def princess_peaches_castle(evil_monster)
+    # puts "#{evil_monster} is trying to kidnap Princess Peach!"
+# end
+
+# princess_peaches_castle(evil_monster)
+
+# def practicing_method_scope
+    # im_trapped_in_the_method = "You can't access me outside this method!"
+# end
+
+# im_trapped_in_the_method
+
+$evil_monster = "Bowser"
+
+def princess_peaches_castle
+    puts "#{$evil_monster} is trying to kidnap Princess Peach!"
+end
+
+princess_peaches_castle
